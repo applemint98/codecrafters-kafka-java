17c6feadf50c668782caa6372dd84c4662f3c79d
AppleMint98 <applemint9821@gmail.com>
2026-06-24 21:12:19 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,6 +18,14 @@ public class Main {
        serverSocket.setReuseAddress(true);
        // Wait for connection from client.
        clientSocket = serverSocket.accept();
+
+       OutputStream out = clientSocket.getOutputStream();
+       byte[] response = new byte[] {
+               0, 0, 0, 0,
+               0, 0, 0, 7
+       };
+       out.write(response);
+       out.flush();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      } finally {
